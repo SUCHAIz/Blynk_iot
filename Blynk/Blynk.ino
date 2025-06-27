b172e1c8bede927a6d0a6fc2d5e33d3eb3daab3e
SUCHAIz <sinthwannthkeiyrtiphumi@gmail.com>
2025-06-27 16:31:49 +0700

    Gauge

--- Blynk/Blynk.ino
+++ Blynk/Blynk.ino
@@ -1,6 +1,6 @@
 #define BLYNK_TEMPLATE_ID "TMPL6R5bNou8E"
-#define BLYNK_TEMPLATE_NAME "Test"
-#define BLYNK_AUTH_TOKEN "Y5lQpXib96Ysm5AQjT-UzR_uJsTMn8UP"
+#define BLYNK_TEMPLATE_NAME "Kiattphum"
+#define BLYNK_AUTH_TOKEN "32ddW1WMPh3qQ9M4jZeRY0iZEQ6Xovje"
 
 
 #include <WiFi.h>
@@ -12,11 +12,26 @@ char pass[] = "Ken123taa";
 
 int ledpin = 2;
 
-BLYNK_WRITE(V0)
+BLYNK_WRITE(V0) // รอรับค่าจาก V0
 {
-  int value = param.asInt();
-  digitalWrite(ledpin, value);
+  int vPin = param.asInt();
+  int pinValue = param.asInt();
+  if (pinValue == 1)
+  {
+   Blynk.virtualWrite(vPin, 1);
+    digitalWrite(ledpin, 1);
+  }
+  else if (pinValue == 0)
+  {
+    Blynk.virtualWrite(vPin, 0);
+    digitalWrite(ledpin, 0);
+  }
+  Serial.print("V0 button value is : ");
+  Serial.println(pinValue);
+  
 }
+
+
 
 void setup() 
 {
@@ -32,7 +47,3 @@ void loop()
   Blynk.run();
 }
 
-asdasdsadasda
-sa
-as
-dsaas
